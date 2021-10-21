c0958e589ffe52f31c42054c2149bb012af6d48d
Shakarim Sapa <info@relitech.kz>
2021-10-21 11:51:28 +0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-check-bold</v-icon>
+    </v-system-bar>
+
+    <v-navigation-drawer app width="300">
+      <v-navigation-drawer absolute color="grey lighten-3" mini-variant>
+        <v-avatar class="d-block text-center mx-auto mt-4" color="grey darken-1" size="36" ></v-avatar>
+
+        <v-divider class="mx-3 my-5"></v-divider>
+
+        <v-avatar v-for="n in 6" :key="n" class="d-block text-center mx-auto mb-9" color="grey lighten-1" size="28" >
+        </v-avatar>
+      </v-navigation-drawer>
+
+      <v-list class="pl-14" shaped>
+        <v-list-item v-for="n in 5" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <ModelerIndex></ModelerIndex>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ModelerIndex from './components/modeler/Index.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+
+    components: {
+      ModelerIndex
+    },
+
+    data: () => ({
+      //
+    }),
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/modeler/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Modeler></Modeler>
+  </div>
+</template>
+
+<script>
+  import Modeler from './Modeler.vue'
+
+  export default {
+    name: 'ModelerIndex',
+    components: {
+      Modeler
+    }
+  };
+</script>
