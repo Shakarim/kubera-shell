a6ea49a5143cb1dd44213a26bb780c6d8cee631c
Shakarim Sapa <info@relitech.kz>
2021-12-04 22:22:18 +0600

    reinit app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,8 @@
 <template>
-  <v-app>
-    <v-system-bar app>
-      <v-spacer></v-spacer>
-      <v-icon>mdi-check-bold</v-icon>
-    </v-system-bar>
-
-    <v-navigation-drawer app width="300">
-      <v-navigation-drawer absolute color="grey lighten-3" mini-variant>
-        <v-avatar class="d-block text-center mx-auto mt-4" color="grey darken-1" size="36" ></v-avatar>
-
-        <v-divider class="mx-3 my-5"></v-divider>
-
-        <v-avatar v-for="n in 6" :key="n" class="d-block text-center mx-auto mb-9" color="grey lighten-1" size="28" >
-        </v-avatar>
-      </v-navigation-drawer>
-
-      <v-list class="pl-14" shaped>
-        <v-list-item v-for="n in 5" :key="n" link>
-          <v-list-item-content>
-            <v-list-item-title>Item {{ n }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <ModelerIndex></ModelerIndex>
-    </v-main>
-  </v-app>
+  <router-view></router-view>
 </template>
 
 <script>
-  import ModelerIndex from './components/modeler/Index.vue'
-
-  export default {
-    name: 'App',
-
-    components: {
-      ModelerIndex
-    },
-
-    data: () => ({
-      //
-    }),
-  };
+export default {
+}
 </script>
